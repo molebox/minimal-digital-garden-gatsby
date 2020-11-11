be2c7dca4811fdc17be60e99f481dd498d59d150
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-11-11 15:01:32 +0100

    moved letters to static folder

--- src/components/cubes/cubes.js
+++ src/components/cubes/cubes.js
@@ -5,15 +5,15 @@ import { Physics } from "@react-three/cannon";
 import Plane from "./plane";
 import Cube from "./cube";
 
-import D from "./../../assets/letters/Letter_D.png";
-import I from "./../../assets/letters/Letter_I.png";
-import G from "./../../assets/letters/Letter_G.png";
-import A from "./../../assets/letters/Letter_A.png";
-import L from "./../../assets/letters/Letter_L.png";
-import N from "./../../assets/letters/Letter_N.png";
-import R from "./../../assets/letters/Letter_R.png";
-import T from "./../../assets/letters/Letter_T.png";
-import E from "./../../assets/letters/Letter_E.png";
+// import D from "/Letter_D.png";
+// import I from "/Letter_I.png";
+// import G from "/Letter_G.png";
+// import A from "/Letter_A.png";
+// import L from "/Letter_L.png";
+// import N from "/Letter_N.png";
+// import R from "/Letter_R.png";
+// import T from "/Letter_T.png";
+// import E from "/Letter_E.png";
 
 /**
  * A set of 3D cubes which spell out "Digital Garden"
@@ -52,20 +52,20 @@ const Cubes = () => {
             />
             <Physics>
               <Plane />
-              <Cube imagePath={[D]} position={[-5, 11, -3]} />
-              <Cube imagePath={[I]} position={[-2, 11, -3]} />
-              <Cube imagePath={[G]} position={[-1, 10, -3]} />
-              <Cube imagePath={[I]} position={[0, 9, -3]} />
-              <Cube imagePath={[T]} position={[1, 8, -3]} />
-              <Cube imagePath={[A]} position={[2, 7, -3]} />
-              <Cube imagePath={[L]} position={[3, 8, -3]} />
+              <Cube imagePath={["/Letter_D.png"]} position={[-5, 11, -3]} />
+              <Cube imagePath={["/Letter_I.png"]} position={[-2, 11, -3]} />
+              <Cube imagePath={["/Letter_G.png"]} position={[-1, 10, -3]} />
+              <Cube imagePath={["/Letter_I.png"]} position={[0, 9, -3]} />
+              <Cube imagePath={["/Letter_T.png"]} position={[1, 8, -3]} />
+              <Cube imagePath={["/Letter_A.png"]} position={[2, 7, -3]} />
+              <Cube imagePath={["/Letter_L.png"]} position={[3, 8, -3]} />
 
-              <Cube imagePath={[G]} position={[-2, 7, 2]} />
-              <Cube imagePath={[A]} position={[-1, 6, 2]} />
-              <Cube imagePath={[R]} position={[0, 5, 2]} />
-              <Cube imagePath={[D]} position={[1, 4, 2]} />
-              <Cube imagePath={[E]} position={[3, 3, 2]} />
-              <Cube imagePath={[N]} position={[4, 2, 2]} />
+              <Cube imagePath={["/Letter_G.png"]} position={[-2, 7, 2]} />
+              <Cube imagePath={["/Letter_A.png"]} position={[-1, 6, 2]} />
+              <Cube imagePath={["/Letter_R.png"]} position={[0, 5, 2]} />
+              <Cube imagePath={["/Letter_D.png"]} position={[1, 4, 2]} />
+              <Cube imagePath={["/Letter_E.png"]} position={[3, 3, 2]} />
+              <Cube imagePath={["/Letter_N.png"]} position={[4, 2, 2]} />
             </Physics>
           </Suspense>
         </Canvas>
