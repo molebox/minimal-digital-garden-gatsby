0cbb7aa986f141d337d71bae6e55ee07f0f64ae9
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-11-11 17:44:59 +0100

    removed loader due to error

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Flex, Box } from "@chakra-ui/core";
-import { Loader } from "@react-three/drei";
 
 const Layout = ({ children }) => {
   return (
@@ -12,13 +11,6 @@ const Layout = ({ children }) => {
         bgColor="brand.bg"
         p={3}
       >
-      <Loader
-        dataInterpolation={(p) => `Planting seeds....${p.toFixed(2)}% done`}
-        innerStyles={{
-          backgroundColor: '#ffffff'
-        }}
-        dataStyles={{color: '#1f2127'}}
-      />
         {children}
       </Flex>
     </Box>
