12f18dfe24475887f905a9187e1659a7795fb23a
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-11-18 11:05:05 +0100

    added basic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Error = () => (
+    <div>
+        <h1>404</h1>
+        <p>I haven't implemented this page yet but as you can tell, something went wrong...</p>
+        </div>
+)
+
+export default Error;
