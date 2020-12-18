bdc69c3ac371939f182570f2920bedda095749b1
HAINES-DESKTOP\richa <hello@richardhaines.dev>
2020-12-18 08:13:06 +0100

    moved btn transitions to outer parent div

--- src/components/mode-button.js
+++ src/components/mode-button.js
@@ -1,34 +1,38 @@
 import React from "react";
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Button, Box, useColorMode } from "@chakra-ui/react";
 
 const ModeButton = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
+    <Box
+    transition="all .25s ease-in-out"
+    boxShadow="-5px 5px #000"
+    _active={{
+      boxShadow: "-2px 2px #000",
+      transform: "translate(-2px, 2px)",
+    }}
+    _hover={{
+      boxShadow: "-2px 2px #000",
+      transform: "translate(-2px, 2px)",
+    }}
+    position="relative"
+    gridColumn={["1 / 4", 3]}
+    gridRow={[2, 1]}
+    m={["30px 0 0 100px", "0 40px 0 0"]}
+    >
     <Button
       bg="brand.bg"
-      m={["30px 0 0 100px", "0 40px 0 0"]}
-      gridColumn={["1 / 4", 3]}
-      gridRow={[2, 1]}
       px={[2]}
-      position="relative"
-      transition="all .25s ease-in-out"
-      boxShadow="-5px 5px #000"
       borderRadius={0}
       variant="outline"
       textTransform="uppercase"
       fontSize="sm"
-      _active={{
-        boxShadow: "-2px 2px #000",
-        transform: "translate(-2px, 2px)",
-      }}
-      _hover={{
-        boxShadow: "-2px 2px #000",
-        transform: "translate(-2px, 2px)",
-      }}
       onClick={toggleColorMode}
+      
     >
       {colorMode === "light" ? " Like it Mustard?" : "Like it Clean?"}
     </Button>
+    </Box>
   );
 };
 
