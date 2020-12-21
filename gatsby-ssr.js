c109d5c4d2cceb7c967d3e96380bda9f5d16cf7a
HAINES-DESKTOP\richa <hello@richardhaines.dev>
2020-12-21 15:31:21 +0100

    removed font import in ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,15 +3,7 @@ import { wrapRootElement as wrap } from './root-wrapper';
 import React from "react"
 import { ColorModeScript } from "@chakra-ui/react"
 
-export const onRenderBody = ({ setPreBodyComponents, setHeadComponents  }) => {
-  setHeadComponents([
-    <link
-      rel="stylesheet"
-      href="https://indestructibletype.com/fonts/Jost.css"
-      type="text/css"
-      charset="utf-8"
-    />
-  ]);
+export const onRenderBody = ({ setPreBodyComponents  }) => {
   setPreBodyComponents([
     <ColorModeScript initialColorMode="light" key="chakra-ui-no-flash" />,
   ]);
